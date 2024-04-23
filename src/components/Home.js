4c121c355780ca273613939154fb5de9f6ac0d14
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-04-23 15:53:11 +0300

    hw

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {city,username} from "../data/user";
+
 
 function Home() {
   return (
@@ -9,3 +11,7 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
+
+// Here are some relevant code fragments from other
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,10 @@
 const username = "Liza";
-const city = "New York";
 
+const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
+
+export {city,image,username}
+
+
 
 // export all three variables as named exports
